ca1a223cd7b78a2e8e976f6b3fce1591f68600d2
Dave Edelhart <dave@wonderlandlabs.com>
2012-09-23 09:09:00 -0700

    improving factory

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
     gate: require('./gate'),
     Gate: require('./gate'),
     mongoose_model: require('./mongoose_model'),
+    mongoose_factory: require('./mongoose_model/factory'),
     nakamura_gate: require('./node_modules/gate'),
 
     proper_path: require('./proper_path'),
--- mongoose_model/factory.js
+++ mongoose_model/factory.js
@@ -1,60 +1,42 @@
 var async = require('async');
 var util = require('util');
 var _ = require('underscore');
+var Gate = require('./../node_modules/gate');
 
-module.exports = function (model, generator, count, callback, dont_return, preserve) {
-    var done = 0;
-    var docs = [];
-    var errs = [];
+module.exports = function (model, generator, count, callback, dontreturn) {
     var aborted = false;
     var killjoy;
 
-    function _make() {
-        console.log('making %s %s s', count, model.name);
-        var index;
-        for (index = 0; index < count; ++index) {
-            var doc = generator(index);
+    console.log('making %s %s s', count, model.name);
+    var gate = Gate.create({failFast:true});
+    for (index = 0; index < count; ++index) {
+        if (aborted) return;
 
-            (function (i) {
-                 function cb(err, record) {
-                //     console.log('written %s', util.inspect(record, true, 3));
-                    if (aborted) {
-                        return;
-                    }
-
-                    if (!dont_return) {
-                        if (err) {
-                            docs[i] = false;
-                            errs[i] = err;
-                        } else {
-                         //   console.log('saving %s', util.inspect(record));
-                            docs[i] = record;
-                            errs[i] = false;
-                        }
-                    } else {
-                        console.log('not returning %s', util.inspect(record));
-                    }
-                    ++done;
-                //    console.log('model %s of %s made', done, count);
-                    if (done >= count) {
-                        callback(null, {docs:docs, errs:errs});
-                        clearTimeout(killjoy);
-                        console.log('DONE MAKING MODELS');
-                    }
-                }
-
-                model.put(doc, cb);
-            })(index);
-
-
+        if (dontreturn) {
+            var latch = gate.latch();
+            model.put(generator(index), function () {
+                latch();
+            });
+        } else {
+            model.put(generator(index), gate.latch());
         }
 
-         killjoy = setTimeout(function () {
-            aborted = true;
-            callback(new Error('factory took too long'));
-        }, 1000 * count);
     }
 
-    _make();
+    gate.await(function (err, res) {
+        if (err) {
+            return cb(err);
+        }
+        callback(null, _.map(res, function (items) {
+            return items[1];
+        }))
+
+    });
+
+    killjoy = setTimeout(function () {
+        aborted = true;
+        callback(new Error('factory took too long'));
+        gate.canceled = true;
+    }, 1000 * count);
 
 }
