ae56e2311375d6e3bbec242c4f9ac91eb1aaf80c
Dave Edelhart <dave@wonderlandlabs.com>
2012-09-23 10:28:41 -0700

    fixing merge error

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ module.exports = {
     gate: require('./gate'),
     Gate: require('./gate'),
     mongoose_model: require('./mongoose_model'),
-<<<<<<< HEAD
     mongoose_factory: require('./mongoose_model/factory'),
-=======
->>>>>>> 2f0a512ddf54ffd08f44fca231acb218a9b8837c
     nakamura_gate: require('./node_modules/gate'),
 
     proper_path: require('./proper_path'),
